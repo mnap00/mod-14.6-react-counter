a5a2e0c9081578021108142c43b6f30917eb1673
mnap <learn@10g.pl>
2018-04-07 22:54:01 +0200

    Add decrement method

--- script.js
+++ script.js
@@ -9,12 +9,23 @@ var Counter = React.createClass({
             counter: this.state.counter + 1
         });
     },
+    decrement: function() {
+        this.setState({
+            counter: this.state.counter - 1
+        });
+    },
     render: function() {
-        return React.createElement('div', {onClick: this.increment},
-            React.createElement('span', {}, 'Counter ' + this.state.counter)
+        return React.createElement('div', {className: 'counter'},
+            React.createElement('span', {}, 'Counter ' + this.state.counter),
+            React.createElement('button', {onClick: this.increment}, '+1'),
+            React.createElement('button', {onClick: this.decrement}, '-1')
         );
     }
 });
 
-var element = React.createElement(Counter);
+var element = React.createElement('div', {className: 'container'},
+    React.createElement(Counter),
+    React.createElement(Counter),
+    React.createElement(Counter),
+);
 ReactDOM.render(element, document.getElementById('app'));
