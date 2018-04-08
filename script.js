53b3ddb8248fb7fc83939f9463b0ef440e3e4f0a
mnap <learn@10g.pl>
2018-04-08 22:22:15 +0200

    Update script with 'console.log'; add gitignore and browser-sync

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
 var Counter = React.createClass({
+    // INITIALISE
+    getDefaultProps: function() {
+        console.log('Set initial value in case not saved on server');
+    },
     getInitialState: function() {
+        console.log('Get from defaults or if saved in browser\'s storage');
         return {
             counter: 0
         }
     },
+    componentWillMount: function() {
+        console.log('Pass some attribute or mode to rendering. ' +
+            'May be based on initial state.');
+    },
+    componentDidMount: function() {
+        console.log('Get from server counter\'s saved state');
+    },
+    // UPDATE
+    componentWillReceiveProps(nextProps) {
+        console.log('Set color if counter reached some value');
+    },
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('Check on server if state reached that value');
+        return true;
+    },
+    componentWillUpdate(nextProps, nextState) {
+        console.log('Pass attribute / mode for rendering');
+    },
+    componentDidUpdate(prevProps, prevState) {
+        console.log('Send value to server');
+    },
+    // REMOVE
+    componentWillUnmount() {
+        console.log('Reset counter to defaults and remove state from server');
+    },
+    // BASE METHODS
     increment: function() {
         this.setState({
             counter: this.state.counter + 1
@@ -20,7 +51,7 @@ var Counter = React.createClass({
             React.createElement('button', {onClick: this.increment}, '+1'),
             React.createElement('button', {onClick: this.decrement}, '-1')
         );
-    }
+    },
 });
 
 var element = React.createElement('div', {className: 'container'},
